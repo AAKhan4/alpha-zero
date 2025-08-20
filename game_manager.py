60677041915679c9800565e90cbf842f3195c28d
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2025-08-21 00:24:52 +0200

    Update game_manager.py

--- game_manager.py
+++ game_manager.py
@@ -1,19 +1,35 @@
 import tic_tac_toe
+import mcts
+import numpy as np
 
 game = tic_tac_toe.TicTacToe()
 
 player = 1
 state = game.get_initial_state()
 
+args = {
+    "num_searches": 1000,
+    "c": 1.41
+}
+
+monte_carlo = mcts.MCTS(game, args)
+
 while 1:
     print(state)
-    valid_actions = game.get_valid_actions(state)
-    print(valid_actions)
-    action = int(input(f"Player {player}, enter your action (0-8): "))
 
-    if valid_actions[action] == 0:
-        print("Invalid action. Try again.")
-        continue
+    if player == 1:
+        print("Player 1's turn (X)")
+        valid_actions = game.get_valid_actions(state)
+        print(valid_actions)
+        action = int(input(f"Player {player}, enter your action (0-8): "))
+
+        if valid_actions[action] == 0:
+            print("Invalid action. Try again.")
+            continue
+    else:
+        neutral_state = game.change_perspective(state, player)
+        mcts_probs = monte_carlo.search(neutral_state)
+        action = np.argmax(mcts_probs)
 
     state = game.get_next_state(state, action, player)
     val, terminal = game.is_terminal(state, action)
