40bfc233f9078a2b0546975bb2f968b74f15af5d
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2025-09-13 01:57:19 +0200

    Update training arguments and add new parameters
    Increased default values for num_searches and num_iterations, and added num_workers, lr, and weight_decay to the training arguments. Also updated ConnectFour-specific num_iterations.

--- training/training_args.py
+++ training/training_args.py
@@ -1,9 +1,9 @@
 class TrainingArgsBuilder:
     def __init__(self, game):
         self.args = {
-            "num_searches": 80,
+            "num_searches": 90,
             "c": 2,
-            "num_iterations": 4,
+            "num_iterations": 5,
             "num_self_play": 500,
             "num_parallel_games": 128,
             "num_epochs": 4,
@@ -12,7 +12,10 @@ class TrainingArgsBuilder:
             "epsilon": 0.25,
             "alpha": 0.3,
             "res_blocks": 4,
-            "channels": 64
+            "channels": 64,
+            "num_workers": 4,
+            "lr": 0.001,
+            "weight_decay": 1e-4
         }
         self.build_args(game)
 
@@ -20,7 +23,7 @@ class TrainingArgsBuilder:
         if game.__class__.__name__ == "ConnectFour":
             self.args.update({
                 "num_searches": 600,
-                "num_iterations": 8,
+                "num_iterations": 10,
                 "num_epochs": 4,
                 "batch_size": 128,
                 "res_blocks": 9,
