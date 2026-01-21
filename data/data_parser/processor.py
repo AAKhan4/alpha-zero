26302f8613b15b5e5793fdc7aa1a7e33c82cb8ef
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2026-01-22 00:21:49 +0100

    Update processor.py
    data augmentation, rotation and flips of state & action for diversity in data

--- data/data_parser/processor.py
+++ data/data_parser/processor.py
@@ -44,6 +44,7 @@ class DataProcessor:
                             continue
                         col = ord(coords[0]) - ord('a')
                         row = ord(coords[1]) - ord('a')
+
                         try:
                             if col < self.game.col_count and row < self.game.row_count:
                                 b.play(row, col, color)
@@ -54,12 +55,16 @@ class DataProcessor:
                         new_state = np.array([[mapping[c] for c in row] for row in b.board], dtype=np.int8)
                         if color == 'w':
                             new_state *= -1  # Perspective of white
-                        all_states.append(self.game.get_encoded_state(new_state))
 
                         action = row * self.game.col_count + col
                         if action >= self.board_size:
                             action = self.board_size  # Pass move
-                        all_actions.append(action)
+
+                        transforms = self.get_all_transforms(new_state, action)
+
+                        for s, a in transforms:
+                            all_states.append(self.game.get_encoded_state(s))
+                            all_actions.append(a)
                 except ValueError:
                     print(f"Invalid move in file {file_name}. Skipping rest of game.")
                     continue
@@ -69,3 +74,25 @@ class DataProcessor:
 
         np.save(os.path.join(self.processed_data_dir, 'states.npy'), states_array)
         np.save(os.path.join(self.processed_data_dir, 'actions.npy'), actions_array)
+
+    def get_all_transforms(self, state: np.ndarray, action: int) -> list[tuple[np.ndarray, int]]:
+        '''Generates all rotations and reflections of the given state and action.'''
+        transforms = []
+        board_dim = (self.game.row_count, self.game.col_count)
+
+        for k in range(4):
+            rotated_state = np.rot90(state.reshape(board_dim), k).flatten()
+            row, col = divmod(action, self.game.col_count)
+            
+            for _ in range(k):
+                row, col = col, self.game.row_count - 1 - row  # Rotate coordinates
+            rotated_action = row * self.game.col_count + col if action < self.board_size else action
+            transforms.append((rotated_state, rotated_action))
+
+            flipped_state = np.fliplr(rotated_state.reshape(board_dim)).flatten()
+
+            flipped_col = self.game.col_count - 1 - col
+            flipped_action = row * self.game.col_count + flipped_col if action < self.board_size else action
+            transforms.append((flipped_state, flipped_action))
+
+        return transforms
