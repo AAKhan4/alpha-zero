70b6a66ed92f1609d97945cf4a54585a9aac9985
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2026-01-21 20:34:24 +0100

    Update data_preprocessing.py

--- training_scripts/data_preprocessing.py
+++ training_scripts/data_preprocessing.py
@@ -1,11 +1,10 @@
 from data.data_parser.processor import DataProcessor
-from games.base_game import BaseGame
 from games.go.go import Go
 import os
 
 # Define directories for raw and processed data
-raw_data_dir = "../data/raw_data/go/9x9"
-processed_data_dir = "../data/processed_data/go/9x9"
+raw_data_dir = "./data/raw_data/go/9x9"
+processed_data_dir = "./data/processed_data/go/9x9"
 
 # Create processed data directory if it doesn't exist
 if not os.path.exists(processed_data_dir):
